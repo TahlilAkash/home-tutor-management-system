0ce5e99cea753f8e8b03632e7eb11a1607facca8
Tahlil Akash <tahlilakash@.com>
2023-12-26 17:37:00 +0600

    teacher post admin dashboard design

--- resources/views/admin/pages/teacher/teacher_list.blade.php
+++ resources/views/admin/pages/teacher/teacher_list.blade.php
@@ -3,59 +3,58 @@
 
 
 @section('content')
+    <div class="container">
+        <h1 style="font-size: 20px; font-weight: bold;">Teacher Post List</h1>
+        <div class="row">
 
-<div class="container">
-    <h1 style="font-size: 20px; font-weight: bold;">Teacher List</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Class Name</th>
+                        <th scope="col">Subject Name</th>
+                        <th scope="col">Salary</th>
+                        <th scope="col">Contact</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($TeacherList as $key => $TList)
+                        <tr>
+                            <th scope="row">{{ $key + 1 }}</th>
+                            <td>{{ $TList->name }}</td>
+                            <td>{{ $TList->role }}</td>
+                            <td>{{ $TList->email }}</td>
+                            <td>{{ $TList->class_list }}</td>
+                            <td>{{ $TList->subject_name }}</td>
+                            <td>{{ $TList->salary }}</td>
+                            <td>{{ $TList->contact }}</td>
+                            <td>{{ $TList->address }}</td>
+                            <td><img class="img" src="{{ url('/uploads/' . $TList->image) }}"
+                                    style="width: 50px; height: 50px; object-fit: cover; border: 1px solid rgb(172, 169, 169); border-radius:60%; color:black;"
+                                    alt="img"></td>
+                            <td>
+                                <span class="post-status {{ $TList->status }}">
+                                    {{ $TList->status }}
+                                </span>
+                            </td>
+                            <td>
+                                <a href="{{ route('teacherlist.edit', $TList->id) }}" class="btn btn-success m-1">Edit</a>
+                                <a href="{{ route('teacherlist.delete', $TList->id) }}" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
 
-
-        <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Role</th>
-            <th scope="col">Email</th>
-            <th scope="col">Class Name</th>
-            <th scope="col">Subject Name</th>
-            <th scope="col">Salary</th>
-            <th scope="col">Contact</th>
-            <th scope="col">Address</th>
-            <th scope="col">Image</th>
-            <th scope="col">Status</th>
-            <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($TeacherList as $key=>$TList)
-            <tr>
-            <th scope="row">{{$key+1}}</th>
-            <td>{{$TList->name}}</td>       
-            <td>{{$TList->role}}</td>
-            <td>{{$TList->email}}</td>
-            <td>{{$TList->class_list}}</td>
-            <td>{{$TList->subject_name}}</td>
-            <td>{{$TList->salary}}</td>
-            <td>{{$TList->contact}}</td>
-            <td>{{$TList->address}}</td>
-            <td><img class="img" src="{{url('/uploads/'.$TList->image)}}" alt="img"></td>
-            <td>
-                <span class="post-status {{$TList->status}}">
-                    {{$TList->status}}
-                </span>
-                </td>
-            <td>
-                <a href="{{route('teacherlist.edit',$TList->id)}}" class="btn btn-success">Edit</a>
-                <a href="{{route('teacherlist.delete',$TList->id)}}" class="btn btn-danger">Delete</a>
-            </td>
-            </tr>
-            @endforeach
-        
-        </tbody>
-        </table>
-
-    
+                </tbody>
+            </table>
         @endsection
-
+    </div>
 </div>
 <style>
     .post-status {
@@ -73,7 +72,8 @@
         color: white;
         background-color: green;
     }
+
+    .table tbody td {
+        padding: 5px;
+    }
 </style>
-
-
-
